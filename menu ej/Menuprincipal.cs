e7bd7ca4fcc4c627798f63c854ca756449ccbb8e
lucianoarn <estoyderuculovich@gmail.com>
2024-06-03 23:47:19 -0300

    .

--- menu ej/Menuprincipal.cs
+++ menu ej/Menuprincipal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Menu
@@ -24,7 +24,7 @@ namespace Menu
             Console.Clear();
             Console.SetCursorPosition(0, 0);
 
-            // Draw the menu names horizontally
+            
             for (int i = 0; i < menus.Count; i++)
             {
                 if (i == currentMenuIndex)
@@ -37,7 +37,7 @@ namespace Menu
                 Console.ResetColor();
             }
 
-            // Draw the selected submenu items vertically
+            
             int columna = currentMenuIndex * 20;
             int fila = 3;
             for (int j = 0; j < menus[currentMenuIndex].items.Length; j++)
@@ -65,11 +65,11 @@ namespace Menu
                 {
                     case ConsoleKey.RightArrow:
                         currentMenuIndex = (currentMenuIndex + 1) % menus.Count;
-                        currentItemIndex = 0; // Reset item index when changing menu
+                        currentItemIndex = 0; 
                         break;
                     case ConsoleKey.LeftArrow:
                         currentMenuIndex = (currentMenuIndex - 1 + menus.Count) % menus.Count;
-                        currentItemIndex = 0; // Reset item index when changing menu
+                        currentItemIndex = 0; 
                         break;
                     case ConsoleKey.DownArrow:
                         currentItemIndex = (currentItemIndex + 1) % menus[currentMenuIndex].items.Length;
@@ -92,7 +92,7 @@ namespace Menu
             string selectedItem = menus[currentMenuIndex].items[currentItemIndex];
             Console.Clear();
             Console.WriteLine($"Has seleccionado: {selectedItem}");
-            Console.WriteLine("Presiona cualquier tecla para volver al menú...");
+            Console.WriteLine("Presiona cualquier tecla para volver al menu...");
             Console.ReadKey();
         }
     }
--- menu ej/Properties/AssemblyInfo.cs
+++ menu ej/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// La información general de un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// La informacion general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la informacion
 // asociada a un ensamblado.
 [assembly: AssemblyTitle("Menu")]
 [assembly: AssemblyDescription("")]
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// Si establece ComVisible en false, los tipos de este ensamblado no estaran visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
 [assembly: ComVisible(false)]
@@ -22,15 +22,15 @@ using System.Runtime.InteropServices;
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
 [assembly: Guid("c11a8798-6459-4b2f-a17c-7c77e08ca645")]
 
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+// La informacion de version de un ensamblado consta de los cuatro valores siguientes:
 //
-//      Versión principal
-//      Versión secundaria
-//      Número de compilación
-//      Revisión
+//      Version principal
+//      Version secundaria
+//      Numero de compilacion
+//      Revision
 //
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
-// utilizando el carácter "*", como se muestra a continuación:
+// Puede especificar todos los valores o usar los valores predeterminados de numero de compilacion y de revision
+// utilizando el caracter "*", como se muestra a continuacion:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
